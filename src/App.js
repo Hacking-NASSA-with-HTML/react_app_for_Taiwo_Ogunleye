1ee17c0ec41fe4bd6aa7b74bbd5097287c7d9243
Hacking-NASSA-with-HTML <follow.trend7777@gmail.com>
2022-12-15 22:44:46 +0400

    feat: add Home component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
     return (
         <div className="App">
             <Navbar />
+            <Home />
             <header className="App-header">
 
                 <p>Good Luck Taiwo Ogunleye!</p>
---
+++ src/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div>
+            <h3>I am cool Home Component</h3>
+            <h3>from file Home js</h3>
+        </div>
+    )
+}
+
+export default Home
